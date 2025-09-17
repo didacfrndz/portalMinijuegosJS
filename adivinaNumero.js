837862f6f442934edd677fb6cdb5abe02bb6b626
didacfrndz <156692497+didacfrndz@users.noreply.github.com>
2025-09-17 20:54:46 +0000

    faltareset

--- adivinaNumero.js
+++ adivinaNumero.js
@@ -3,6 +3,7 @@ const comprobar = document.querySelector(`#btncomprobar`);
 const texto = document.querySelector(`#txttexto`);
 const intentos = document.querySelector(`#divIntentos`);
 const cont = document.querySelector(`#divContador`);
+const btnReiniciar = `<button>Reiniciar</button>`;
 let contador = 0;
 let min = 1;
 let max = 20;
@@ -15,21 +16,27 @@ function comrpobacion() {
     let NUMERO = document.querySelector(`#numero`).value;
     NUMERO = parseInt(NUMERO);
     console.log(NUMERO);
-
-    if (NUMERO < min || NUMERO > max) {
+    if(contador===5){
+        texto.innerHTML = `<p>Maximo de intentos permitidos</p>  ${btnReiniciar}`; 
+    }else if (NUMERO < min || NUMERO > max) {
         texto.innerHTML = `<p>Ingresa un número entre ${min} y ${max}</p>`;
     } else if (NUMERO < numeroRandom) {
         texto.innerHTML = `<p>El número es mayor que ${NUMERO}</p>`;
         min = NUMERO +1;
+        contador++;
+        cont.innerHTML = `<p>Intento ${contador} </p>`;
     } else if (NUMERO > numeroRandom) {
         texto.innerHTML = `<p>El número es menor que ${NUMERO}</p>`;
         max = NUMERO -1;
+        contador++;
+        cont.innerHTML = `<p>Intento ${contador} </p>`;
     } else if (NUMERO === numeroRandom) {
-        texto.innerHTML = `<p>¡Felicidades! Has adivinado el número.</p>`;
+        texto.innerHTML = `<p>¡Felicidades! Has adivinado el número.</p> ${btnReiniciar}`;
+        
     }
+        intentos.innerHTML += `<div id="estilo">${NUMERO}</div>`;
 }
 
-
-
-
-   
+btnReiniciar.addEventListener('click', function() {
+    location.reload();
+});
